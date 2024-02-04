1ef8010534a8bd890f81f2b044f022242b324428
정회민 <wony1102@naver.com>
2024-02-04 19:25:02 +0900

    add collaborator
    nest g res 명령어로 생성.
    collaborator: 이 서비스의 사용자

--- collab-talk-server/src/app.module.ts
+++ collab-talk-server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CollaboratorModule } from './collaborator/collaborator.module';
 
 @Module({
-  imports: [],
+  imports: [CollaboratorModule],
   controllers: [AppController],
   providers: [AppService],
 })
