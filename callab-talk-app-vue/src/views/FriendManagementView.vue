5df86483879b5f70772eadf5582e63b05952d71c
정회민 <wony1102@naver.com>
2024-03-18 17:39:47 +0900

    style(FriendManagementView.vue): 커스텀 테이블 제거 및 el-table 사용
    el-table-v2 사용 하려고 변경 했으나 아직 beta 버전이어서 사용에 문제가 발생할 수 있을 것 같아 el-table 로 변경.

--- callab-talk-app-vue/src/views/FriendManagementView.vue
+++ callab-talk-app-vue/src/views/FriendManagementView.vue
@@ -1,33 +1,26 @@
 <script setup lang="ts">
-import { computed, onBeforeMount, ref } from "vue";
-import CollabTable from "@/components/collabTable/CollabTable.vue";
-import {
-  CallabTableData,
-  CollabTableHeader,
-} from "@/components/collabTable/types";
+import { onBeforeMount, ref } from "vue";
 import {
   getFriendListByNickName,
   IFriend,
 } from "@/apis/friendManagementView/friendManagementViewApi";
 
 const searchKeyword = ref("");
-const tableHeader: CollabTableHeader[] = [];
-const tableData = ref<CallabTableData[]>([]);
+
+const tableData = ref<IFriend[]>([]);
 const friendList = ref<IFriend[]>([]);
 
 const getTableDataByKeyword = () => {
   const keyword = searchKeyword.value;
-  const dataList = keyword
+  tableData.value = keyword
     ? friendList.value.filter(
         (data) => data.name === keyword || data.nickName === keyword
       )
     : friendList.value;
-  tableData.value = dataList.map((data) => {
-    return [
-      { key: "nickName", label: data.nickName },
-      { key: "imgSource", label: data.imgSource },
-    ];
-  });
+};
+
+const requestAddFriend = (id: string) => {
+  console.log(id);
 };
 
 onBeforeMount(() => {
@@ -48,16 +41,35 @@ onBeforeMount(() => {
       size="large"
       placeholder="검색하기"
     />
-    <CollabTable
-      class="friend-table"
-      show-header
-      :header="tableHeader"
-      :data="tableData"
-    >
-      <template #description>
-        <div>온라인 - {{ tableData.length }}명</div>
-      </template>
-    </CollabTable>
+    <div class="search-result">
+      <el-table
+        class="search-result-table"
+        :data="tableData"
+        row-class-name="data-row"
+        :show-header="false"
+      >
+        <el-table-column align="center" width="70px">
+          <template #default="scope">
+            <el-avatar
+              shape="circle"
+              :size="40"
+              :src="scope.row.imgSource"
+              fit="contain"
+            />
+          </template>
+        </el-table-column>
+        <el-table-column prop="nickName" width="150px" />
+        <el-table-column width="200px">
+          <template #default="scope">
+            <el-button
+              class="hover-button"
+              @click="requestAddFriend(scope.row.id)"
+              >친구추가요청
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -70,12 +82,31 @@ onBeforeMount(() => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
   .search-input {
     width: 100%;
     padding: 30px 30px 10px;
   }
-  .friend-table {
+  .search-result {
+    width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
+    text-align: left;
+    .search-result-table {
+      width: calc(100% - 60px);
+      height: calc(100% - 30px);
+      .data-row {
+        .hover-button {
+          visibility: hidden;
+        }
+      }
+      .data-row:hover {
+        .hover-button {
+          visibility: visible;
+        }
+      }
+    }
   }
 }
 </style>
