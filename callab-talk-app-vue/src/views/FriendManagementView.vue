4af91152be0f3767013531e84e7184776cfd03ae
정회민 <wony1102@naver.com>
2024-03-21 09:14:18 +0900

    fix(친구 추가 기능): ID 입력 값이 공백일때에는 요청 안하도록 수정.

--- callab-talk-app-vue/src/views/FriendManagementView.vue
+++ callab-talk-app-vue/src/views/FriendManagementView.vue
@@ -48,6 +48,8 @@ const addFriendById = () => {
   const target = addFriendInput.value;
   addFriendResult.value = { title: "", type: "none" };
 
+  if (target === "") return;
+
   requestFriendById(addFriendInput.value).then((res) => {
     if (res) {
       addFriendResult.value.type = "success";
